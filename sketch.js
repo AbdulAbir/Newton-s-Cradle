fd79ff3f41403abbbd2a13ecc0d45df9defc4b0d
Abdul Abir <71123123+AbdulAbir@users.noreply.github.com>
2020-10-30 15:51:08 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,77 +1,60 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
-const Constraint = Matter.Constraint;
-const MouseConstraint = Matter.MouseConstraint
 const Mouse = Matter.Mouse;
+const Constraint = Matter.Constraint;
 
-function preload()
-{
-	
-}
+const MouseConstraint = Matter.MouseConstraint;
+
+var world, engine, canvas;
+var pendulum1, pendulum2, pendulum3, pendulum4, pendulum5;
+var sling1, sling2, sling3, sling4, sling5;
+var mConstraint;
 
 function setup() {
-	canvas=createCanvas(windowWidth/2,windowHeight/1.5);
-	engine = Engine.create();
-	world = engine.world;
+  canvas = createCanvas(windowWidth / 2, windowHeight / 1.5);
+  engine = Engine.create();
+  world = engine.world;
 
   let canvasmouse = Mouse.create(canvas.elt);
   canvasmouse.pixelRatio = pixelDensity();
   let options = {
     mouse: canvasmouse
-  }
-  mConstraint = MouseConstraint.create(engine,options);
+  };
+  mConstraint = MouseConstraint.create(engine, options);
   World.add(world, mConstraint);
 
-	//Creating the Bodies .
-	pendulum1 = new pendulum(500,500,0);
-	pendulum2 = new pendulum(550,500,0);
-	pendulum3 = new pendulum(600,500,0);
-	pendulum4 = new pendulum(650,500,0);
-	pendulum5 = new pendulum(700,500,0);
-	roofObject = new roof(600,200,300,20)
-	sling1 = new sling(pendulum1,{x:500,y:450});
-	sling2 = new sling(pendulum2,{x:550,y:450});
-	sling3 = new sling(pendulum3,{x:600,y:450});
-	sling4 = new sling(pendulum4,{x:650,y:450});
-	sling5 = new sling(pendulum5,{x:700,y:450});
+  pendulum1 = new Pendulum(340, 450, "#00b0ff");
+  pendulum2 = new Pendulum(400, 450, "#e91e63");
+  pendulum3 = new Pendulum(460, 450, "#ffc107");
+  pendulum4 = new Pendulum(520, 450, "#e91e63");
+  pendulum5 = new Pendulum(580, 450, "#00b0ff");
 
-	Engine.run(engine);
-  
+  sling1 = new Sling(pendulum1.body, { x: 340, y: 200 });
+  sling2 = new Sling(pendulum2.body, { x: 400, y: 200 });
+  sling3 = new Sling(pendulum3.body, { x: 460, y: 200 });
+  sling4 = new Sling(pendulum4.body, { x: 520, y: 200 });
+  sling5 = new Sling(pendulum5.body, { x: 580, y: 200 });
 }
 
-
 function draw() {
-  rectMode(CENTER);
-  background(120);
+  background(0);
+  Engine.update(engine);
   pendulum1.display();
   pendulum2.display();
   pendulum3.display();
   pendulum4.display();
   pendulum5.display();
-  roofObject.display();
   sling1.display();
   sling2.display();
   sling3.display();
   sling4.display();
   sling5.display();
-
-  drawSprites();
-
- 
 }
 
-function keyPressed() 
-{
- if (keyCode === UP_ARROW) 
-  {
-   Matter.Body.setPosition( pendulum1.body,{ x: mouseX, y: mouseY})
-    
-  }
+function mouseDragged() {
+  Matter.Body.setPosition(pendulum1.body, { x: mouseX, y: mouseY });
 }
-
 
 
 
